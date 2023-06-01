5a30ff07935937c2362f00c0cfc4df944bfba318
XDancer <111144908+andylebeau@users.noreply.github.com>
2023-06-01 16:28:45 -0400

    Display working and added start of SCRIPT

---
+++ script.js
@@ -0,0 +1,33 @@
+let display = document.querySelector('.display');
+
+const userInput = document.querySelectorAll('button')
+    userInput.forEach((digitPressed) => {
+        digitPressed.addEventListener('click', () => {
+            if (digitPressed.value == '') {
+                doOperator(display.textContent)
+            }
+            display.textContent = display.textContent + digitPressed.value;
+        })
+    })
+
+function doOperator (equation) {
+    let displayResult = ''
+    const [x, y] = equation.split(/[+,-,x,÷]/)
+    const operation = equation.match(/[^0-9]/)[0]
+    switch (operation) {
+        case '+':
+            displayResult = x + y;
+            break;
+        case '-':
+            displayResult = x - y;
+            break;
+        case 'x':
+            displayResult = x * y;
+            break;
+        case '÷':
+            displayResult = x / y;
+        default:
+            displayResult = 'ERROR'
+    }
+    display.textContent = displayResult;
+}
